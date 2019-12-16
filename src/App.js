6587a488f219c95ad027cc8ae50e21db2fad8e3f
Ian Castillo <i.michaelcastillo@gmail.com>
2019-12-15 20:56:42 -0600

    imported components + routing from react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+//imported React
 import React from "react";
-import Header from "./components/Header.js";
 
+//imported components from src folder
+import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+
+// Basic Routing
+import {Route, Link} from 'react-router-dom';
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Link to ='/'>Home</Link>
+      <Link to ='/characters'>Characters</Link>
+
+      <Route exact path='/' component={WelcomePage}/>
+      <Route path='/characters' component ={CharacterList}/> 
     </main>
   );
 }
